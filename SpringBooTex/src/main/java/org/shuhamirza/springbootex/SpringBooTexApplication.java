50da7c8d0d1ee80e75d406c5f57c9945b9ba17ac
shuhanmirza <shuhan.mirza@gmail.com>
2023-12-26 16:36:07 +0600

    Initiate Spring Boot Project

---
+++ SpringBooTex/src/main/java/org/shuhamirza/springbootex/SpringBooTexApplication.java
@@ -0,0 +1,13 @@
+package org.shuhamirza.springbootex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooTexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBooTexApplication.class, args);
+    }
+
+}
