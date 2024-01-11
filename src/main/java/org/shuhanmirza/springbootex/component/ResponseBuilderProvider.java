6a0dbbe37bd430af488ed1eea8bba6865ee40826
shuhanmirza <shuhan.mirza@gmail.com>
2024-01-11 17:00:45 +0600

    Implement pdf file response builder

--- src/main/java/org/shuhanmirza/springbootex/component/ResponseBuilderProvider.java
+++ src/main/java/org/shuhanmirza/springbootex/component/ResponseBuilderProvider.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.shuhanmirza.springbootex.component.jsonresponsebuilder.JsonResponseBuilder;
 import org.shuhanmirza.springbootex.component.jsonresponsebuilder.OctetStreamResponseBuilder;
+import org.shuhanmirza.springbootex.component.jsonresponsebuilder.PdfFileResponseBuilder;
 import org.shuhanmirza.springbootex.enums.ResponseType;
 import org.springframework.stereotype.Component;
 
@@ -18,11 +19,13 @@ import org.springframework.stereotype.Component;
 public class ResponseBuilderProvider {
     private final JsonResponseBuilder jsonResponseBuilder;
     private final OctetStreamResponseBuilder octetStreamResponseBuilder;
+    private final PdfFileResponseBuilder pdfFileResponseBuilder;
 
     public ResponseBuilder getResponseBuilder(ResponseType responseType) {
         return switch (responseType) {
             case JSON -> jsonResponseBuilder;
             case OCTET_STREAM -> octetStreamResponseBuilder;
+            case PDF -> pdfFileResponseBuilder;
             default -> throw new RuntimeException(String.format("NO RESPONSE BUILDER FOR TYPE %s", responseType));
         };
     }
